ff03bd897f79074c5155ffff711f6f8ba7f7d401
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2015-04-21 17:21:21 +0200

    Add cs_CZ locale

---
+++ lang/cs/lang.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Správce souborů',
+        'description' => 'Správce souborů a složek',
+        'author' => 'Gergő Szabó'
+    ],
+    'error' => [
+        'text' => 'Nejdříve musíte nainstalovat následující pluginy:',
+        'plugin' => 'Wysiwyg Editors'
+    ],
+    'widget' => [
+        'basic' => [
+            'label' => 'Přehled uložených souborů',
+            'folders' => 'Složky',
+            'files' => 'Soubory',
+            'size' => 'Celková velikost'
+        ],
+        'type' => [
+            'label' => 'Přehled uložených souborů dle typu',
+            'audio' => 'Audio',
+            'archive' => 'Archivy',
+            'code' => 'Kód',
+            'doc' => 'Dokumenty',
+            'image' => 'Obrázky',
+            'other' => 'Jiné',
+            'prezi' => 'Prezentace',
+            'table' => 'Tabulky',
+            'text' => 'Texty',
+            'video' => 'Video'
+        ],
+        'pieces' => '{0}položek|{1}položka|[2,Inf]položky',
+        'limit' => 'Limit velikosti'
+    ],
+    'shortcuts' => [
+        'cut' => 'vyjmout',
+        'copy' => 'kopírovat',
+        'paste' => 'vložit',
+        'delete' => 'smazat',
+        'select' => 'vybrat vše',
+        'upload' => 'nahrát',
+        'info' => 'zobrazit inforamce',
+        'search' => 'vyhledat',
+        'main_folder' => 'domovská složka',
+        'parent_folder' => 'nadřazená složka',
+        'open' => 'otevřít',
+        'view' => 'zobrazit',
+        'back' => 'zpět',
+        'forward' => 'vpřed',
+        'new_folder' => 'nová složka',
+        'reload' => 'obnovit',
+        'download' => 'stáhnout',
+        'rename' => 'přejmenovat',
+        'first_item' => 'první položka',
+        'last_item' => 'poslední položka',
+        'up' => 'UP',
+        'down' => 'DOWN',
+        'left' => 'LEFT',
+        'right' => 'RIGHT'
+    ]
+];
