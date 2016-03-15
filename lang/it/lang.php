36e484705ffffcef336eee980ac6691669a914d0
Szabó Gergő <gergo85@gmail.com>
2016-03-15 11:27:17 +0100

    Minor bugfix

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -14,7 +14,7 @@ return [
         'tooltip2' => 'Se vuoi nascondere anche il link Media nel menù in alto, scarica dall\'October Marketplace il plugin <b>Backend Plus</b>.',
         'tooltip3' => 'Per favore <b>dai una valutazione a questo plugin</b> se ti ha aiutato nel tuo lavoro:',
         'hide_stat' => 'Nascondi le statistiche del plugin File Manager.',
-        'hide_help' => 'Nascondi l'aiuto del plugin File Manager.'
+        'hide_help' => 'Nascondi l\'aiuto del plugin File Manager.'
     ],
     'widget' => [
         'basic' => [
